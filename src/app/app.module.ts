d83f2c74937c819541f412091b5ebd68793598d3
MukulMairh <msoni7718@gmail.com>
2021-01-03 22:50:40 +0530

    Separeated all tabs, implemented routing, page not found

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,38 @@ import { AppComponent } from './app.component';
 import { ServerComponent } from './Server/server.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
+
+
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent, WarningAlertComponent, SuccessAlertComponent],
-  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, ServerComponent, WarningAlertComponent, SuccessAlertComponent, PageNotFoundComponent, DataBindingComponent],
+  imports: [BrowserModule, FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: AppComponent
+      },
+      {
+        path: 'warning',
+        component: WarningAlertComponent
+      },
+      {
+        path: 'success',
+        component: SuccessAlertComponent
+      },
+      {
+        path: 'dataBinding',
+        component: DataBindingComponent
+      },
+      {
+        path: '**',
+        component: PageNotFoundComponent
+      }
+    ])],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/success-alert/success-alert.component.ts
+++ src/app/success-alert/success-alert.component.ts
@@ -6,26 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./success-alert.component.scss']
 })
 export class SuccessAlertComponent implements OnInit {
-  usageOfInterpolation="Interpolation is used to send data from ts to front screen.";
-  myEvent="No event exists!!";
-  interpolationThroughMethod ()
-  {
+  usageOfInterpolation = "Interpolation is used to send data from ts to front screen.";
+  myEvent = "No event exists!!";
+  newEvent = "Birthday Event";
+  interpolationThroughMethod() {
     return "Interpolation can also be performed using a method that returns string"
   }
-  delayMyButton=false;
-  constructor() { 
+  delayMyButton = false;
+  constructor() {
 
-   setTimeout(()=>{
-     this.delayMyButton=true;
-   },3000)   
+    setTimeout(() => {
+      this.delayMyButton = true;
+    }, 3000)
 
   }
 
   ngOnInit(): void {
   }
-  onEventBindingCreation(){
-    this.myEvent="Event is created successfully.";
+  onEventBindingCreation() {
+    this.myEvent = "Event is created successfully.";
 
   }
- 
+  addNewInput(event: any) {
+    console.log(event);
+    // At the time of 2 way binding we don't need the next line written 
+    this.newEvent = event.target.value;
+  }
 }
